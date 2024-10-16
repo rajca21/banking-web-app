a2609352ac44222522a014bc5d9ec19b4bbd1d78
rajca21 <2nikolar1@gmail.com>
2024-10-16 14:37:10 +0200

    build config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    chunkSizeWarningLimit: 5000, // Set the limit to 1MB (1000 kB)
+  },
+});
